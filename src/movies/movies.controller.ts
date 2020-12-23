8d3f5f29bf6602544332fc447ae2b75dfc18a787
hands8142 <hands8142@naver.com>
2020-12-23 10:53:10 +0900

    movie api

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,43 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { Movie } from './entities/Movie.entity';
+import { MoviesService } from './movies.service';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+
+@Controller('movies')
+export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
+  @Get()
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
+  }
+
+  @Get('/:id')
+  getOne(@Param('id') id: number): Movie {
+    return this.moviesService.getOne(id);
+  }
+
+  @Post()
+  create(@Body() data: CreateMovieDto) {
+    return this.moviesService.create(data);
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') id: number) {
+    return this.moviesService.deleteOne(id);
+  }
+
+  @Patch('/:id')
+  path(@Param('id') id: number, @Body() data: UpdateMovieDto) {
+    return this.moviesService.update(id, data);
+  }
+}
